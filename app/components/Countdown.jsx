f9770771f0a24e05baecd715c3fb21dd16648f13
SFujimoto <sfujimoto@ss-MacBook-Air.local>
2016-09-04 23:25:53 +0900

    Add test for contact form

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Clock from './Clock';
+import CountdownForm from './CountdownForm';
 
 
 class Countdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+  handleSetCountdown(seconds) {
+    this.setState({
+      count: seconds
+    });
+  }
   render() {
+    let {count} = this.state;
     return (
       <div>
-        <Clock totalSeconds={129} />
+        <Clock totalSeconds={count} />
+        <CountdownForm onSetCountdown={this.handleSetCountdown.bind(this)}/>
       </div>
     );
   }
