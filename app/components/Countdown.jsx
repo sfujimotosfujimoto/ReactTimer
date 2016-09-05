1219560b99be976d7aad78a63142fd147d2b3bd6
SFujimoto <sfujimoto@ss-MacBook-Air.local>
2016-09-05 10:20:09 +0900

    aa

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -7,12 +7,38 @@ class Countdown extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
+      countdownStatus: 'stopped'
     }
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        default:
+
+      }
+    }
+  }
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      let newCount = this.state.count -1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+
+      
+    }, 1000)
+  }
+
   handleSetCountdown(seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'started'
     });
   }
   render() {
